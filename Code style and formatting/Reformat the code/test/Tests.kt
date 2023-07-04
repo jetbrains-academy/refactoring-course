96cbfff72adc04205431a16f994c7bb2c0d700ef
Anna Potriasaeva <anchouls@Annas-MacBook-Pro.local>
2023-07-04 12:42:31 +0200

    Fixed comments

--- Code style and formatting/Reformat the code/test/Tests.kt
+++ Code style and formatting/Reformat the code/test/Tests.kt
@@ -1,25 +1,38 @@
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory
-import org.junit.Assert
-import org.junit.Test
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 
 class Test {
-    @Test
-    fun testSolution() {
-        val fixtureFactory = IdeaTestFixtureFactory.getFixtureFactory()
-        val fixtureBuilder = fixtureFactory.createFixtureBuilder("Fxt")
-        val fixture = fixtureFactory.createCodeInsightFixture(fixtureBuilder.fixture)
-        fixture.testDataPath = System.getProperty("user.dir")
-        fixture.setUp()
-        val project = fixture.project
-        val virtualFile = fixture.copyFileToProject("/src/Task.kt")
-        val psiFile = ReadAction.compute<PsiFile?, RuntimeException> {
-            PsiManager.getInstance(project).findFile(virtualFile)
+
+    companion object {
+
+        private lateinit var project: Project
+        private lateinit var psiFile: PsiFile
+
+        @JvmStatic
+        @BeforeAll
+        fun initialize() {
+            val fixtureFactory = IdeaTestFixtureFactory.getFixtureFactory()
+            val fixtureBuilder = fixtureFactory.createFixtureBuilder("Fxt")
+            val fixture = fixtureFactory.createCodeInsightFixture(fixtureBuilder.fixture)
+            fixture.testDataPath = System.getProperty("user.dir")
+            fixture.setUp()
+            project = fixture.project
+            val virtualFile = fixture.copyFileToProject("/src/Task.kt")
+            psiFile = ReadAction.compute<PsiFile?, RuntimeException> {
+                PsiManager.getInstance(project).findFile(virtualFile)
+            }
         }
+    }
+    @Test
+    fun testSolutionIsFormatted() {
         var originalCode = ""
         var formattedCode = ""
 
@@ -30,6 +43,6 @@ class Test {
             formattedCode = psiFile.text
         }
 
-        Assert.assertTrue("Code is not formatted.", originalCode == formattedCode)
+        Assertions.assertEquals(formattedCode, originalCode)
     }
 }
--- Renaming code/Fix typos/test/Tests.kt
+++ Renaming code/Fix typos/test/Tests.kt
@@ -1,26 +1,40 @@
 import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiRecursiveElementWalkingVisitor
 import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory
 import org.jetbrains.kotlin.psi.KtNamedFunction
-import org.junit.Assert
-import org.junit.Test
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 
 class Test {
-    @Test fun testSolution() {
-        val fixtureFactory = IdeaTestFixtureFactory.getFixtureFactory()
-        val fixtureBuilder = fixtureFactory.createFixtureBuilder("Fxt")
-        val fixture = fixtureFactory.createCodeInsightFixture(fixtureBuilder.fixture)
-        fixture.testDataPath = System.getProperty("user.dir")
-        fixture.setUp()
-        val project = fixture.project
-        val virtualFile = fixture.copyFileToProject("/src/Task.kt")
-        val psiFile = ReadAction.compute<PsiFile?, RuntimeException> {
-            PsiManager.getInstance(project).findFile(virtualFile)
-        }
 
+    companion object {
+
+        private lateinit var project: Project
+        private lateinit var psiFile: PsiFile
+
+        @JvmStatic
+        @BeforeAll
+        fun initialize() {
+            val fixtureFactory = IdeaTestFixtureFactory.getFixtureFactory()
+            val fixtureBuilder = fixtureFactory.createFixtureBuilder("Fxt")
+            val fixture = fixtureFactory.createCodeInsightFixture(fixtureBuilder.fixture)
+            fixture.testDataPath = System.getProperty("user.dir")
+            fixture.setUp()
+            project = fixture.project
+            val virtualFile = fixture.copyFileToProject("/src/Task.kt")
+            psiFile = ReadAction.compute<PsiFile?, RuntimeException> {
+                PsiManager.getInstance(project).findFile(virtualFile)
+            }
+        }
+    }
+
+    @Test
+    fun testTyposInSolution() {
         val setNames = mutableSetOf<String>()
         ReadAction.compute<Unit?, RuntimeException> {
             psiFile.accept(object : PsiRecursiveElementWalkingVisitor() {
@@ -34,6 +48,6 @@ class Test {
             })
         }
         val setCorrectNames = setOf("getGradeStatus", "setName", "setAge", "setAverageScore")
-        Assert.assertTrue("There are typos in the file.", setNames.equals(setCorrectNames))
+        Assertions.assertEquals(setCorrectNames, setNames)
     }
 }
