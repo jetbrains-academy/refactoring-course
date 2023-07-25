27f696cedbc79a3eb573ae4d3ff51de812623181
Anna Potriasaeva <anchouls@pc-13-172.customer.ask4.lan>
2023-07-25 21:54:12 +0200

    Added tests for inlining refactoring

--- InliningCode/InlineMethodAndVariables/src/main/kotlin/jetbrains/refactoring/course/inlining/Task.kt
+++ InliningCode/InlineMethodAndVariables/src/main/kotlin/jetbrains/refactoring/course/inlining/Task.kt
@@ -6,18 +6,10 @@ import java.io.PrintWriter
 class Store {
     public fun calculateAveragePrice(productPrice: List<Int>): Int? {
         return try {
-            val totalPrice = productPrice.sum()
-            val numberOfProducts = productPrice.count()
-            totalPrice / numberOfProducts
+            productPrice.sum() / productPrice.count()
         } catch (error: Exception) {
-            logError(error, "Exception.txt")
+            PrintWriter(File("Exception.txt"), Charsets.UTF_8).use { it.print(error.toString()) }
             null
         }
-    }
-
-    private fun logError(error: Exception, filePath: String) {
-        val file = File(filePath)
-        val text = error.toString()
-        PrintWriter(file, Charsets.UTF_8).use { it.print(text) }
     }
 }
--- InliningCode/InlineMethodAndVariables/test/Tests.kt
+++ InliningCode/InlineMethodAndVariables/test/Tests.kt
@@ -1,9 +1,69 @@
-import org.junit.Assert
-import org.junit.Test
+import org.jetbrains.academy.test.system.test.BaseIjTestClass
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import java.io.File
 
-class Test {
-    @Test fun testSolution() {
-        //TODO: implement your test here
-        Assert.assertTrue("Tests not implemented for the task", false)
+class InliningTest : BaseIjTestClass() {
+
+    companion object {
+
+        private lateinit var sourceText: String
+
+        @JvmStatic
+        @BeforeAll
+        fun initialize() {
+            val taskDirectoryPath = System.getProperty("user.dir")
+            val sourceCodeFile =
+                File("$taskDirectoryPath/src/main/kotlin/jetbrains/refactoring/course/inlining/Task.kt")
+            sourceText = sourceCodeFile.readText()
+        }
+    }
+
+    @Test
+    fun testDeletedVariablesAndMethods() {
+        setUp()
+        myFixture.configureByText("Task.kt", sourceText)
+        Assertions.assertFalse(hasProperty("totalPrice")) {
+            "Please, identify unnecessary variables"
+        }
+        Assertions.assertFalse(hasProperty("numberOfProducts")) {
+            "Please, identify unnecessary variables"
+        }
+        Assertions.assertFalse(hasProperty("file")) {
+            "Please, identify unnecessary variables"
+        }
+        Assertions.assertFalse(hasProperty("text")) {
+            "Please, identify unnecessary variables"
+        }
+        Assertions.assertFalse(hasMethod("logError")) {
+            "Please, identify unnecessary methods"
+        }
+    }
+
+    private fun checkExpressionCallIsReplacedByItsBody(
+        expression: String,
+        parent: String,
+        isParentTypeFunction: Boolean = false
+    ) {
+        Assertions.assertTrue(hasExpressionWithParent(expression, parent, isParentTypeFunction)) {
+            "Please, replace method or variable call by its body - $expression"
+        }
+    }
+
+    @Test
+    fun testExpressionCallIsReplacedByItsBody() {
+        setUp()
+        myFixture.configureByText("Task.kt", sourceText)
+        checkExpressionCallIsReplacedByItsBody("productPrice.sum()", "productPrice.sum() / productPrice.count()")
+        checkExpressionCallIsReplacedByItsBody("productPrice.count()", "productPrice.sum() / productPrice.count()")
+        checkExpressionCallIsReplacedByItsBody("File(\"Exception.txt\")", "File(\"Exception.txt\")")
+        checkExpressionCallIsReplacedByItsBody("error.toString()", "error.toString()")
+        checkExpressionCallIsReplacedByItsBody(
+            "PrintWriter(File(\"Exception.txt\"), Charsets.UTF_8).use { it.print(error.toString()) }",
+            "calculateAveragePrice",
+            true
+        )
+
     }
 }
