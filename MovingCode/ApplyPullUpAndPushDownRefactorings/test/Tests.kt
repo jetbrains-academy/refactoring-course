68da694e3a6c058965384e3d2c9150c5c9333a03
Anna Potriasaeva <anchouls@pc-13-172.customer.ask4.lan>
2023-07-27 14:49:51 +0200

    Updated tests

--- MovingCode/ApplyPullUpAndPushDownRefactorings/test/Tests.kt
+++ MovingCode/ApplyPullUpAndPushDownRefactorings/test/Tests.kt
@@ -2,72 +2,23 @@ import org.junit.jupiter.api.Test
 
 class PullUpAndPushDownTest {
     @Test
-    fun vector2ClassTest() {
-        val clazz = vector2Class.checkBaseDefinition()
-        vector2Class.checkFieldsDefinition(clazz)
-        vector2Class.checkDeclaredMethods(clazz)
+    fun animalClassTest() {
+        val clazz = animalClass.checkBaseDefinition()
+        animalClass.checkFieldsDefinition(clazz)
+        animalClass.checkDeclaredMethods(clazz)
     }
 
     @Test
-    fun contextClassTest() {
-        val clazz = contextClass.checkBaseDefinition()
-        contextClass.checkFieldsDefinition(clazz)
-        contextClass.checkDeclaredMethods(clazz)
+    fun catClassTest() {
+        val clazz = catClass.checkBaseDefinition()
+        catClass.checkFieldsDefinition(clazz)
+        catClass.checkDeclaredMethods(clazz)
     }
 
     @Test
-    fun controllerClassTest() {
-        val clazz = controllerClass.checkBaseDefinition()
-        controllerClass.checkFieldsDefinition(clazz)
-        controllerClass.checkDeclaredMethods(clazz)
-    }
-
-    @Test
-    fun playerControllerClassTest() {
-        val clazz = playerControllerClass.checkBaseDefinition()
-        playerControllerClass.checkFieldsDefinition(clazz)
-        playerControllerClass.checkDeclaredMethods(clazz)
-    }
-
-    @Test
-    fun enemyControllerClassTest() {
-        val clazz = enemyControllerClass.checkBaseDefinition()
-        enemyControllerClass.checkFieldsDefinition(clazz)
-        enemyControllerClass.checkDeclaredMethods(clazz)
-    }
-
-    @Test
-    fun keyboardPlayerControllerClassTest() {
-        val clazz = keyboardPlayerControllerClass.checkBaseDefinition()
-        keyboardPlayerControllerClass.checkFieldsDefinition(clazz)
-        keyboardPlayerControllerClass.checkDeclaredMethods(clazz)
-    }
-
-    @Test
-    fun mousePlayerControllerClassTest() {
-        val clazz = mousePlayerControllerClass.checkBaseDefinition()
-        mousePlayerControllerClass.checkFieldsDefinition(clazz)
-        mousePlayerControllerClass.checkDeclaredMethods(clazz)
-    }
-
-    @Test
-    fun characterClassTest() {
-        val clazz = characterClass.checkBaseDefinition()
-        characterClass.checkFieldsDefinition(clazz)
-        characterClass.checkDeclaredMethods(clazz)
-    }
-
-    @Test
-    fun enemyClassTest() {
-        val clazz = enemyClass.checkBaseDefinition()
-        enemyClass.checkFieldsDefinition(clazz)
-        enemyClass.checkDeclaredMethods(clazz)
-    }
-
-    @Test
-    fun playerClassTest() {
-        val clazz = playerClass.checkBaseDefinition()
-        playerClass.checkFieldsDefinition(clazz)
-        playerClass.checkDeclaredMethods(clazz)
+    fun dogClassTest() {
+        val clazz = dogClass.checkBaseDefinition()
+        dogClass.checkFieldsDefinition(clazz)
+        dogClass.checkDeclaredMethods(clazz)
     }
 }
