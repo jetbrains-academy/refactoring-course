7891700e2e63a2a91a27d12944b0966974e4ba91
Anna Potriasaeva <anchouls@pc-13-172.customer.ask4.lan>
2023-07-27 14:11:00 +0200

    Changed task

--- MovingCode/ApplyPullUpAndPushDownRefactorings/src/main/kotlin/jetbrains/refactoring/course/moving/controller/Controller.kt
+++
@@ -1,8 +0,0 @@
-package jetbrains.refactoring.course.moving.controller
-
-import jetbrains.refactoring.course.moving.model.Character
-
-abstract class Controller(private val speed: Int, private val character: Character) {
-
-    abstract fun move()
-}
--- MovingCode/ApplyPullUpAndPushDownRefactorings/src/main/kotlin/jetbrains/refactoring/course/moving/utils/Context.kt
+++
@@ -1,3 +0,0 @@
-package jetbrains.refactoring.course.moving.utils
-
-data class Context(val pressedKeys: Set<String>)
