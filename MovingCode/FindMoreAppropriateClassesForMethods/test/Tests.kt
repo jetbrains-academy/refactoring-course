894b7adbe2051dfd8b424b947b57d6b3664db552
anchouls <potryasaeva.anna@mail.ru>
2023-07-31 16:16:43 +0200

    Updated tests in Moving section

--- MovingCode/FindMoreAppropriateClassesForMethods/test/Tests.kt
+++ MovingCode/FindMoreAppropriateClassesForMethods/test/Tests.kt
@@ -1,8 +1,62 @@
+import org.jetbrains.academy.test.system.test.BaseIjTestClass
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import java.io.File
 
-class MovingTest {
+class MovingTest : BaseIjTestClass() {
+
+    companion object {
+
+        private lateinit var taskDirectoryPath: String
+
+        @JvmStatic
+        @BeforeAll
+        fun initialize() {
+            taskDirectoryPath = System.getProperty("user.dir")
+        }
+    }
+
+    @Test
+    fun testCarClassMoved() {
+        Assertions.assertTrue(
+            File("$taskDirectoryPath/src/main/kotlin/jetbrains/refactoring/course/moving/car/Car.kt").exists()
+        ) {
+            "Please, move the \"Car\" class to a separate file"
+        }
+    }
+
+    @Test
+    fun testDriverClassMoved() {
+        Assertions.assertTrue(
+            File("$taskDirectoryPath/src/main/kotlin/jetbrains/refactoring/course/moving/driver/Driver.kt").exists()
+        ) {
+            "Please, move the \"Driver\" class to a separate file"
+        }
+    }
+
+    @Test
+    fun testStartMethodMovedToCarClass() {
+        val carFile = File("$taskDirectoryPath/src/main/kotlin/jetbrains/refactoring/course/moving/car/Car.kt")
+        val sourceText = carFile.readText()
+        setUp()
+        myFixture.configureByText("Task.kt", sourceText)
+        Assertions.assertTrue(hasMethod("start")) {
+            "Please, move the \"start\" method"
+        }
+    }
+
+    @Test
+    fun testStopMethodMovedToCarClass() {
+        val carFile = File("$taskDirectoryPath/src/main/kotlin/jetbrains/refactoring/course/moving/car/Car.kt")
+        val sourceText = carFile.readText()
+        setUp()
+        myFixture.configureByText("Task.kt", sourceText)
+        Assertions.assertTrue(hasMethod("stop")) {
+            "Please, move the \"stop\" method"
+        }
+    }
+
     @Test
     fun driverClassTest() {
         val clazz = driverClass.checkBaseDefinition()
@@ -15,16 +69,5 @@ class MovingTest {
         val clazz = carClass.checkBaseDefinition()
         carClass.checkFieldsDefinition(clazz)
         carClass.checkDeclaredMethods(clazz)
-    }
-
-    @Test
-    fun newFilesCreatedTest() {
-        val taskDirectoryPath = System.getProperty("user.dir")
-        val mainFile = File("$taskDirectoryPath/src/main/kotlin/jetbrains/refactoring/course/moving/Main.kt")
-        val driverFile = File("$taskDirectoryPath/src/main/kotlin/jetbrains/refactoring/course/moving/driver/Driver.kt")
-        val carFile = File("$taskDirectoryPath/src/main/kotlin/jetbrains/refactoring/course/moving/car/Car.kt")
-        Assertions.assertTrue(mainFile.exists())
-        Assertions.assertTrue(driverFile.exists())
-        Assertions.assertTrue(carFile.exists())
     }
 }
