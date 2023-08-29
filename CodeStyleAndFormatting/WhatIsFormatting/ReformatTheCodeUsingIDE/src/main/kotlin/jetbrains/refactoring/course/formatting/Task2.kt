2677686591904b5d1d3d1df0be17645f0dcd15d0
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2023-08-29 17:48:28 +0200

    Fix typos

--- CodeStyleAndFormatting/WhatIsFormatting/ReformatTheCodeUsingIDE/src/main/kotlin/jetbrains/refactoring/course/formatting/Task2.kt
+++ CodeStyleAndFormatting/WhatIsFormatting/ReformatTheCodeUsingIDE/src/main/kotlin/jetbrains/refactoring/course/formatting/Task2.kt
@@ -1,7 +1,7 @@
 package jetbrains.refactoring.course.formatting
 
 fun funWithFormattingIssues() {
-    println("This function is definitely has formatting issues")
+    println("This function definitely has formatting issues")
     println("... that could be easily fixed using one shortcut")
     for (i in 1..10) {
         println("Please, format me!")
