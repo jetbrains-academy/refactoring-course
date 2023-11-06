982e5ab6dd947191852362a7855a34d0a9f2cead
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-11-06 16:18:52 +0400

    fix: add a reference solution

--- RefactoringToDesignPatterns/StrategyPatternPractice/Task/src/main/kotlin/jetbrains/refactoring/course/patterns/Main.kt
+++ RefactoringToDesignPatterns/StrategyPatternPractice/Task/src/main/kotlin/jetbrains/refactoring/course/patterns/Main.kt
@@ -1,11 +1,16 @@
 package jetbrains.refactoring.course.patterns
 
-fun main() {
-    val order1 = Order(100.0, "CreditCard")
-    val order2 = Order(50.0, "PayPal")
-    val order3 = Order(200.0, "Bitcoin")
+import jetbrains.refactoring.course.patterns.processor.PaymentProcessor
+import jetbrains.refactoring.course.patterns.strategy.BitcoinPayment
+import jetbrains.refactoring.course.patterns.strategy.CreditCardPayment
+import jetbrains.refactoring.course.patterns.strategy.PayPalPayment
 
-    order1.processPayment()
-    order2.processPayment()
-    order3.processPayment()
+fun main() {
+    val creditCardPaymentProcessor = PaymentProcessor(CreditCardPayment())
+    val paypalPaymentProcessor = PaymentProcessor(PayPalPayment())
+    val bitcoinPaymentProcessor = PaymentProcessor(BitcoinPayment())
+
+    creditCardPaymentProcessor.processOrderPayment(100.0)
+    paypalPaymentProcessor.processOrderPayment(50.0)
+    bitcoinPaymentProcessor.processOrderPayment(200.0)
 }
--- RefactoringToDesignPatterns/StrategyPatternPractice/Task/src/main/kotlin/jetbrains/refactoring/course/patterns/Order.kt
+++ RefactoringToDesignPatterns/StrategyPatternPractice/Task/src/main/kotlin/jetbrains/refactoring/course/patterns/Order.kt
@@ -1,20 +1,4 @@
 package jetbrains.refactoring.course.patterns
 
-class Order(val totalAmount: Double, val paymentMethod: String) {
-    fun processPayment() {
-        when (paymentMethod) {
-            "CreditCard" -> {
-                println("Processing credit card payment: $totalAmount")
-            }
-            "PayPal" -> {
-                println("Processing PayPal payment: $totalAmount")
-            }
-            "Bitcoin" -> {
-                println("Processing Bitcoin payment: $totalAmount")
-            }
-            else -> {
-                println("Invalid payment method")
-            }
-        }
-    }
-}
+// The Order class is no longer necessary
+class Order(val totalAmount: Double)
