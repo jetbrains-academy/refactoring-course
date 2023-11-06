9bd60e6170128e7365f0623fc3720cafbe80dc7b
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-11-06 16:48:03 +0400

    fix: fix main files

--- RefactoringToDesignPatterns/FactoryMethodPatternPractice/Task/src/main/kotlin/jetbrains/refactoring/course/patterns/Main.kt
+++ RefactoringToDesignPatterns/FactoryMethodPatternPractice/Task/src/main/kotlin/jetbrains/refactoring/course/patterns/Main.kt
@@ -2,13 +2,6 @@ package jetbrains.refactoring.course.patterns
 
 fun main(args: Array<String>) {
     val transportName = args[0]
-    val transport = if (transportName == "car") {
-        Car()
-    } else if (transportName == "bicycle") {
-        Bicycle()
-    } else {
-        throw IllegalArgumentException("Unknown transport")
-    }
-    val driveMessage = transport.drive()
+    val driveMessage = TransportationServiceFactory().getTransportation(transportName).drive()
     println(driveMessage)
 }
