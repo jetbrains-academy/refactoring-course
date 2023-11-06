2aceb19faafd260f48999892b133b60b2e7818ee
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-11-06 16:27:26 +0400

    fix: fix naming

--- RefactoringToDesignPatterns/StrategyPatternPractice/Task/src/main/kotlin/jetbrains/refactoring/course/patterns/Main.kt
+++ RefactoringToDesignPatterns/StrategyPatternPractice/Task/src/main/kotlin/jetbrains/refactoring/course/patterns/Main.kt
@@ -6,11 +6,11 @@ import jetbrains.refactoring.course.patterns.strategy.CreditCardPayment
 import jetbrains.refactoring.course.patterns.strategy.PayPalPayment
 
 fun main() {
-    val creditCardPaymentProcessor = PaymentProcessor(CreditCardPayment())
-    val paypalPaymentProcessor = PaymentProcessor(PayPalPayment())
-    val bitcoinPaymentProcessor = PaymentProcessor(BitcoinPayment())
+    val creditCardPayment = PaymentProcessor(CreditCardPayment())
+    val paypalPayment = PaymentProcessor(PayPalPayment())
+    val bitcoinPayment = PaymentProcessor(BitcoinPayment())
 
-    creditCardPaymentProcessor.processOrderPayment(100.0)
-    paypalPaymentProcessor.processOrderPayment(50.0)
-    bitcoinPaymentProcessor.processOrderPayment(200.0)
+    creditCardPayment.processOrderPayment(100.0)
+    paypalPayment.processOrderPayment(50.0)
+    bitcoinPayment.processOrderPayment(200.0)
 }
