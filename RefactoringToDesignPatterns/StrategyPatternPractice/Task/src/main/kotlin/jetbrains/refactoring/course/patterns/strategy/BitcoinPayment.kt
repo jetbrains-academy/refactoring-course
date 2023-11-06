ffd093e8ab68d2e7e38ac52de84ab4b13f71572b
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-11-06 16:18:52 +0400

    fix: add a reference solution

--- RefactoringToDesignPatterns/StrategyPatternPractice/Task/src/main/kotlin/jetbrains/refactoring/course/patterns/strategy/BitcoinPayment.kt
+++ RefactoringToDesignPatterns/StrategyPatternPractice/Task/src/main/kotlin/jetbrains/refactoring/course/patterns/strategy/BitcoinPayment.kt
@@ -1,3 +1,5 @@
 package jetbrains.refactoring.course.patterns.strategy
 
-// TODO
+class BitcoinPayment: PaymentStrategy {
+    override fun processPayment(amount: Double) = println("Processing Bitcoin payment: $amount")
+}
--- RefactoringToDesignPatterns/StrategyPatternPractice/Task/src/main/kotlin/jetbrains/refactoring/course/patterns/strategy/CreditCardPayment.kt
+++ RefactoringToDesignPatterns/StrategyPatternPractice/Task/src/main/kotlin/jetbrains/refactoring/course/patterns/strategy/CreditCardPayment.kt
@@ -1,3 +1,5 @@
 package jetbrains.refactoring.course.patterns.strategy
 
-// TODO
+class CreditCardPayment: PaymentStrategy {
+    override fun processPayment(amount: Double) = println("Processing credit card payment: $amount")
+}
