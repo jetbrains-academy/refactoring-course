7fb1dc98c5f21bf837d046c9da7f3bd5f5a24d9a
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2024-01-19 18:56:21 +0100

    Update gradle intellij plugin to 1.16.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jetbrains.intellij") version "1.14.1"
+    id("org.jetbrains.intellij") version "1.16.1"
     java
     kotlin("jvm") version "1.8.20" apply true
     application
